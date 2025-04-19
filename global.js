d2bdda676401e762c853af009417b59494cb070c
CONNIELIUGH <col024@ucsd.edu>
2025-04-18 17:48:37 -0700

    Lab 3 - Finished step 3

--- global.js
+++ global.js
@@ -4,15 +4,48 @@ function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
   }
   
-let navLinks = $$("nav a");
+// let navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-);
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+// );
 
-currentLink?.classList.add("current");
+// currentLink?.classList.add("current");
 
 
 
+let pages = [
+    {url: "", title: "Home"},
+    {url: "projects/", title:"Projects"},
+    {url: "resume/", title: "Resume"},
+    {url: "contact/", title: "Contact"},
+    {url: "https://github.com/CONNIELIUGH", title: "GitHub Profile"}
+]
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages){
+    let url = p.url;
+    let title = p.title;
+    const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+            ? "/"                  // Local server
+            : "/website/";         // GitHub Pages repo name
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    // nav.insertAdjacentHTML('beforeend', `<a href = "${url}">${title}</a>`);
+    let a = document.createElement("a");
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    a.classList.toggle("current", 
+        a.host === location.host && a.pathname === location.pathname,
+    );
+    // GitHub Profile Page direct to new page
+    if (a.host  !== location.host){
+        a.target = "_blank";
+    }
+
+}
 
 
